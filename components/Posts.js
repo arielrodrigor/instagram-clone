4e5f678598a841c6b70c134be92fe6daf93449a3
Ariel Ramirez <ariel.ramirez.reyes@gmail.com>
2023-02-15 17:41:32 -0600

    upload a post and image with firestore and real time firebase

--- components/Posts.js
+++ components/Posts.js
@@ -1,29 +1,28 @@
 import React from 'react';
 import Post from "@/components/Post";
-const DUMMY_DATA = [
-    {
-        id : 1,
-        username : 'johndoe',
-        userImg : 'https://links.papareact.com/3ke',
-        img : 'https://links.papareact.com/3ke',
-        caption : 'This is dope!',
-    },
-    {
-        id : 2,
-        username : 'johndoe',
-        userImg : 'https://links.papareact.com/3ke',
-        img : 'https://links.papareact.com/3ke',
-        caption : 'This is dope!',
-    }
-];
+import { db, storage } from "../firebase";
+import {collection, onSnapshot, query, orderBy} from "firebase/firestore";
 
 function Posts( ) {
+     const [posts, setPosts] = React.useState([]);
+            React.useEffect(() => {
+                return onSnapshot(query(collection(db, 'posts'), orderBy('timestamp', 'desc')), (snapshot) => {
+                    setPosts(snapshot.docs);
+                });
+            }, [db]);
+
     return (
         <div>
             {
-                DUMMY_DATA.map((post) => (
-                    <Post key={post.id} id={post.id} username={post.username} userImg={post.userImg} img={post.img} caption={post.caption} />
-                ))
+                posts.map((post) => (
+                    <Post
+                        key={post.id}
+                        id={post.id}
+                        username={post.data().username}
+                        userImg={post.data().profileImg}
+                        img={post.data().image}
+                        caption={post.data().caption}
+                    />))
             }
 
 
